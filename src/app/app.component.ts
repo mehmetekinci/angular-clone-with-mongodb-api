446d7d576c0fa6657b1a44c01e35220a195f2a2f
mehmetekinci <mehmetekinci1989@gmail.com>
2021-07-26 20:10:04 +0200

    used api service in the components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, Input } from "@angular/core";
+import { ApiService } from "src/services/api.service";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  title = 'angular-clone-with-mongodb-api';
+  title = "angular-clone-with-mongodb-api";
+
+  @Input() public header: object[] = [];
+  @Input() public slider: object[];
+  @Input() public features: [[]];
+  @Input() public about: object;
+  @Input() public contact: object;
+  @Input() public footer: string;
+
+  public article;
+
+  constructor(public apiService: ApiService) {}
+
+  ngOnInit(): void {
+    this.apiService.getDetails().subscribe((data) => {
+      this.article = data[0];
+
+      this.header = data[0].header;
+      this.slider = data[0].slider;
+      this.features = data[0].features;
+      this.about = data[0].about;
+      this.contact = data[0].contact;
+      this.footer = data[0].footer;
+
+      console.log(this.article);
+      console.log(this.header);
+      console.log(this.features);
+      console.log(this.about);
+      console.log(this.contact);
+      console.log(this.footer);
+    });
+  }
 }
--- src/app/primary-features/primary-features.component.ts
+++ src/app/primary-features/primary-features.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, Input, OnInit } from "@angular/core";
 
 @Component({
   selector: "app-primary-features",
@@ -6,6 +6,7 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./primary-features.component.css"],
 })
 export class PrimaryFeaturesComponent implements OnInit {
+  @Input() public feature;
   constructor() {}
 
   ngOnInit(): void {}
